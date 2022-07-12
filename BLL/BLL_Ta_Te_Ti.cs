05899fffafa24998d4ad9bca0ecdbeed6fd657cd
Franco Villafañe <FrancoMiguel.Villafane@alumnos.uai.edu.ar>
2022-07-12 20:56:13 -0300

    a ver

--- BLL/BLL_Ta_Te_Ti.cs
+++ BLL/BLL_Ta_Te_Ti.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abstract;
+using BE;
+using Mapper;
 
 namespace BLL
 {
-    internal class BLL_Ta_Te_Ti
+    public class BLL_Ta_Te_Ti : BLL_Juego, IGameable<BE_Ta_Te_Ti>
     {
+        MPP_Ta_Te_Ti oMPP_Ta_Te_Ti;
+        public BLL_Ta_Te_Ti()
+        {
+            oMPP_Ta_Te_Ti = new MPP_Ta_Te_Ti();
+        }
+
+        public bool GuardarPartida(BE_Ta_Te_Ti Juego)
+        {
+            return oMPP_Ta_Te_Ti.GuardarPartida(Juego);
+        }
+
+        public bool Instrucciones(BE_Ta_Te_Ti Juego)
+        {
+            return oMPP_Ta_Te_Ti.Instrucciones(Juego);
+        }
+
+        public List<BE_Ta_Te_Ti> Listar()
+        {
+            return oMPP_Ta_Te_Ti.Listar();
+        }
+
+        public override void PuntuarPartida()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Mapper/MPP_Ta_Te_Ti.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstract;
+using BE;
+
+namespace Mapper
+{
+    public class MPP_Ta_Te_Ti : IGameable<BE_Ta_Te_Ti>
+    {
+        public bool GuardarPartida(BE_Ta_Te_Ti Juego)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Instrucciones(BE_Ta_Te_Ti Juego)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BE_Ta_Te_Ti> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
